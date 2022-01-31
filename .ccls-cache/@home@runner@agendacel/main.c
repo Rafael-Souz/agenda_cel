67ea81bf29baf4ed50a0e1a318f2a87c5ae83985
Rafael Silva de Souza <73540128+Rafael-Souz@users.noreply.github.com>
2022-01-31 23:58:30 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@agendacel/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct celular{
+  char nome [20];
+  char numero [11];
+  
+};
+int main(void) {
+  struct celular p[20];
+  int i;
+//inicializa o for, para digitar 3 contatos
+  for(i=0; i<3; i++){
+     printf("Digite o seu nome!\n ");
+     gets(p[i].nome);
+     printf("Digite o seu número!\n ");
+     gets(p[i].numero);
+  }
+  // mostrar 3 contatos
+  for(i=0; i<3; i++){
+      printf("Nome: %s\n ",p[i].nome);
+      printf("Número: %s\n ",p[i].numero);
+
+  }
+  return 0;
+}
