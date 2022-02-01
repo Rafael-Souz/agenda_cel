070feb65a33c6362d55803db6b2bde56b0dfab50
Rafael Silva de Souza <73540128+Rafael-Souz@users.noreply.github.com>
2022-02-01 00:13:46 +0000

    primeiro commit

--- main.c
+++ main.c
@@ -4,12 +4,11 @@
 struct celular{
   char nome [20];
   char numero [11];
-  
 };
 int main(void) {
   struct celular p[20];
   int i;
-//inicializa o for, para digitar 3 contatos
+  //for, para digitar 3 contatos
   for(i=0; i<3; i++){
      printf("Digite o seu nome!\n ");
      gets(p[i].nome);
