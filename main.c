d9462c7a0a40df931d017baf4caa270b8d94b87a
Rafael Silva de Souza <73540128+Rafael-Souz@users.noreply.github.com>
2022-02-04 23:44:21 +0000

    mais commit

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 struct celular{
   char nome [20];
   char numero [11];
+  char email [30];
 };
 int main(void) {
   struct celular p[20];
@@ -14,11 +15,14 @@ int main(void) {
      gets(p[i].nome);
      printf("Digite o seu número!\n ");
      gets(p[i].numero);
+     printf("Digite o seu e-mail!\n ");
+     gets(p[i].email);
   }
   // mostrar 3 contatos
   for(i=0; i<3; i++){
       printf("Nome: %s\n ",p[i].nome);
       printf("Número: %s\n ",p[i].numero);
+      printf("E-mail: %s\n ",p[i].email);
 
   }
   return 0;
